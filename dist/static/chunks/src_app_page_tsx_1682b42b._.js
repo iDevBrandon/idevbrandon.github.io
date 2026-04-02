f9cbc6c83e138a74029894c40910ef8cc7981fa9
Brandon Ha <idevbrandon@gmail.com>
2026-04-02 07:39:07 -0400

    update

--- dist/static/chunks/src_app_page_tsx_1682b42b._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_7f39793f._.js",
-  "static/chunks/node_modules__pnpm_495cfe3e._.js"
-],
-    source: "dynamic"
-});
